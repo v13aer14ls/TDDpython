1533c257139f48694bd28c429d09a5883bcab259
v13aer14ls <v13aer14ls@gmail.com>
2020-07-28 17:59:03 -0300

    adicionando unitest

--- funcional_tests.py
+++ funcional_tests.py
@@ -1,8 +1,42 @@
 from selenium import webdriver
+import unittest
 
 
-browser = webdriver.Firefox()
 
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        #Guilherme acessa o site e ve sua homepage
+        self.browser.get('http://localhost:8000')
+
+        # Ele ve se acessou o site que ele quer acessar , se é um site de TO-DO listas
+        self.assertIn('To-Do' , self.browser.title)
+        self.fail('FINISH the test!')
+    
+        #Ele é convidado a inserir um item de tarefa imediatamente
+
+        #Ele digita "aprender testes funcionais" em uma caixa de texto
+
+        #QUando ele aperta Enter a pagina é atualizada e agora possui
+        # "1:aprender testes funcionais" como em uma lista normal
+
+        #Ele agora insere um novo item "user testes funcionais para facilitar minha vida"
+
+        # Agora a pagina é atualizada e mostra os dois itens em sua lista
+
+        #Guilherme se pergunta se o site se lembrará de sua lista. Entao ele nota
+        #que o site gerou um URL unico para mim -- há um pequeno texto
+        #explicando isso.
+
+        #Ele acessa a url e sua lista está la
+
+
+        #Satisfeito ele volta a dormir
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
